874964ee9757668e904da9c10575920be1b463a2
Alejo Morell <34201397+Yukics@users.noreply.github.com>
2023-07-29 23:03:33 +0200

    reconocimiento de imagenes en video funcionando

--- lib/recognize.py
+++ lib/recognize.py
@@ -4,81 +4,71 @@ import numpy as np
 import os
 
 def startRecon(video, image):
+    logging.info("Comenzando el proceso de reconocimiento durante el video")
 
     try:
-        vidcap = cv2.VideoCapture(video) # Cargamos el video
-        template = cv2.imread(image,0) # Cargamos la imagen
-        template = cv2.cvtColor(template, cv2.COLOR_BGR2GRAY)
+        # Cargamos el video
+        vidcap = cv2.VideoCapture(video)
+        # Cargamos la captura de alguno de los frames 
+        template = cv2.imread(image,0) 
+
     except:
         logging.error("No ha sido posible cargar o el video o la imagen proporcionadas, revisa el path")
+    
+    # Contador para saber en que frame estamos iterando
+    count = 0 
 
-    count = 0
-
-    while True:
-      success, image = vidcap.read()
+    # Mientras vidcap nos deje leer el stream de video realizaremos las siguientes acciones
+    while (vidcap.isOpened()): 
+      # Leemos un frame
+      success, image = vidcap.read() 
       
       if not success: 
-        logging.error('Algo ha fallado a la hora leer un fotograma')
-        break         # Si dejamos de poder leer el vídeo paramos
+        # Si dejamos de poder leer el vídeo paramos
+        logging.warning('Algo ha fallado a la hora leer un fotograma o ha finalizado el procesamiento del video')
+        break         
 
-    #   logging.info('Read a new frame: {} {}'.format(success, count))
-      processImage(image, template, count)
+      # Comparamos nuestra captura con el frame del video
+      processImage(image, template, count) 
       count += 1
 
-def processImage(img_rgb, template, count):
-
-    method = cv2.TM_SQDIFF_NORMED
-
-    # Read the images from the file
-    small_image = template
-    # large_image = img_rgb
-    large_image = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
-
-    print(small_image, large_image)
-
-    result = cv2.matchTemplate(small_image, large_image, method)
-    # We want the minimum squared difference
-    mn,_,mnLoc,_ = cv2.minMaxLoc(result)
-
-    # Draw the rectangle:
-    # Extract the coordinates of our best match
-    MPx,MPy = mnLoc
-
-    # Step 2: Get the size of the template. This is the same size as the match.
-    trows,tcols = small_image.shape[:2]
-
-    # Step 3: Draw the rectangle on large_image
-    cv2.rectangle(large_image, (MPx,MPy),(MPx+tcols,MPy+trows),(0,0,255),2)
-
-    # Display the original image with the rectangle around the match.
-    cv2.imshow('output',large_image)
-
-    # The image is only displayed if we call this
-    cv2.waitKey(0)
-
-
-
-
-
-
-
-
-
-
-
-    # img_gray = img_rgb
-    # # img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
+def processImage(frame, template, count):
     
-    # w, h = template.shape[::-1]
-
-    # res = cv2.matchTemplate(img_gray,template,cv2.TM_CCOEFF_NORMED)
-    # threshold = 0.8
-    # loc = np.where( res >= threshold)
-    # for pt in zip(*loc[::-1]):
-    #     cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0,0,255), 2)
+    # Convertimos de rgb a escala de grises el frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     
-    # if not os.path.exists('processing'):
-    #     os.makedirs('processing')
+    # Medimos el ancho y alto del frame
+    w,h = template.shape[0], template.shape[1]
+    
+    # Comprobamos que la captura está en el fotograma
+    matched = cv2.matchTemplate(gray,template,cv2.TM_CCOEFF_NORMED)
+    
+    # Establecemos el grado de fiabilidad necesario de 0 min a 1 max, 99.9% en este caso necesario
+    threshold = .999
+    
+    # Aplicamos el grado de fiabilidad al resultado de la comprobación de la captura en el frame
+    loc = np.where( matched >= threshold)
+    
+    # Cuando comprobamos el grado de fiabilidad, y existe una coincidencia casi exacta, estas arrays no estarán vacías
+    if np.any(loc[0]) and np.any(loc[1]):
+        logging.info('Localizada coincidencia con el frame n {}'.format(count))
+        
+        # Procedemos a generar el recuadro y a mostrar por pantalla la coincidencia ademas de al directorio processed
+        for pt in zip(*loc[::-1]):
+            cv2.rectangle(frame, pt, (pt[0] + w, pt[1] + h), (0,255,255), 2)
 
-    # # This will write different res.png for each frame. Change this as you require
-    # cv2.imwrite('./processing/res{0}.png'.format(count),img_rgb)   
+        # Si no existe el directorio processed lo creamos
+        if not os.path.exists("./processed"):
+            os.makedirs("./processed")
+
+        # Escribimos la imagen en el disco con el recuadro de la coincidencia
+        cv2.imwrite('./processed/res{0}.png'.format(count),frame)
+
+        # Cambiamos el tamaño de la imagen que aparecerá por pantalla
+        imS = cv2.resize(frame, (960, 540)) 
+
+        # Mostramos por pantalla la imagen
+        cv2.imshow('Frame n {}'.format(count) ,imS)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.waitKey(1)
